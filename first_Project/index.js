613f688f02cc8352417130c4ee842200a8d2a666
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-10-25 17:53:20 +0500

    rendering method

---
+++ first_Project/index.js
@@ -0,0 +1,11 @@
+ReactDOM.render(<p>I am Danish</p>, document.getElementById('Fname'))
+ReactDOM.render(
+<ol>
+    <h3>My cricket ideal</h3>
+    <li>Babar Azam</li>
+    <li>Rohit Sharma</li>
+    <li>Virat Kohli</li>
+    <li>Haris Rauf</li>
+</ol>, 
+document.getElementById('fav_list')
+)
