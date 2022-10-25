9e87ebe5ecb3c4010ffbcf52b91cecbd37ffe277
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-10-26 00:58:03 +0500

    few changes

--- first_Project/index.js
+++ first_Project/index.js
@@ -1,7 +1,41 @@
-import HeaderComponent from "./HeaderComponent"
-import MainComponent from "./MainComponent"
-import FooterComponent from "./FooterComponent"
+function HeaderComponent(){
+    return (
+        <header>
+            <nav className = "nav"> 
+                <img src="images/react_logo.png" className = "logo"/>
+                <ul className = "nav-items">
+                    <li>Pricing</li>
+                    <li>Documentation</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
 
+function MainComponent(){
+    return (
+        <div className = "main-div">
+            <h1> Fun Fact About React</h1>
+        <ul>
+            <li>React was first released in 2013</li>
+            <li>React is a JavaScript library</li>
+            <li>React is a front-end library</li>
+            <li>React is a library for building user interfaces</li>
+            <li>It is maintained by Facebook</li>
+        </ul>
+        </div>
+    )
+}
+
+function FooterComponent(){
+    return (
+        <footer className = "footer">
+            <small>@ 2022 Daani development. All rights reserved </small>
+        </footer>
+    )
+} 
 {/** Render the page components with Composable*/}
 function Page(){
 return (
