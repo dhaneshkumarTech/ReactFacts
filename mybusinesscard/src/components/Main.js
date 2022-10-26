3a4068081cf4dbbf8edc54476b2e17108724bc4d
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-10-26 19:29:17 +0500

    main component of business card is completed

--- mybusinesscard/src/components/Main.js
+++ mybusinesscard/src/components/Main.js
@@ -2,7 +2,23 @@ import React from "react";
 
 function Main(){
     return (
-        <h2>About and Interest</h2>
+        <div className="main-component">
+            <div className="personal-info">
+                <h2>Dr AllahBux Mahar</h2>
+                <h4>General Physician</h4>
+                <a href="#"><p>drallahbux.website</p></a>
+                <button className="email-btn"><img src='images/gmail-logo.png' alt='Gmail logo'></img>Gmail</button>
+                <button className="facebook-btn"><img src='images/facebook-logo.png' alt='Facebook logo'></img>Facebook</button>
+            </div>
+            <div className="more-info">
+                <h3>About</h3>
+                <p><small>I am General practitioners and family physician diagnose and treat the diseases, physiological disorders and injuries of patients. I provide primary contact and continuous care toward the management of patients' health. I work in private practice, including group or team practices, hospitals and clinics</small></p>
+                <h3>Interests</h3>
+                <p><small>I like work activities that have to do with ideas and thinking. I search for facts and figure out solutions to problems mentally. I also have social interests. I like work activities that assist others and promote learning and personal development. I like to communicate with others: to teach, give advice, help, or otherwise be of service to others.</small></p>
+                <h3>Hobbies</h3>
+                <p><small>I like to read books, watch movies, listen to music, play games, and do puzzles. I also like to spend time with my family and friends. I like to go to the movies, go out to eat, and go shopping. I like to go to the beach, go camping, and go hiking. I like to go to the park, go to the zoo, and go to museums.</small></p>
+            </div>
+        </div>
     )
 }
 
--- mybusinesscard/src/components/PersonalInfo.js
+++ mybusinesscard/src/components/PersonalInfo.js
@@ -4,11 +4,6 @@ function PersonalInfo(){
     return (
         <div className="profile-div">
             <img src="images/profilepic.jpg" alt="Dhanesh Kumar Profile" className="profile-pic"></img>
-            <h2>Dr AllahBux Mahar</h2>
-            <h4>General Physician</h4>
-            <a href="#"><p>drallahbux.website</p></a>
-            <button className="email-btn"><img src='images/gmail-logo.png' alt='Gmail logo'></img>Gmail</button>
-            <button className="facebook-btn"><img src='images/facebook-logo.png' alt='Facebook logo'></img>Facebook</button>
         </div>
     )
 }
