ccedf3e59b85fd8ba344ff286764fda84ad545b8
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-10-26 18:45:57 +0500

    Profile component completed

--- mybusinesscard/src/components/PersonalInfo.js
+++ mybusinesscard/src/components/PersonalInfo.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function PersonalInfo(){
     return (
-        <div>
-            <img src=""></img>
+        <div className="profile-div">
+            <img src="images/profilepic.jpg" alt="Dhanesh Kumar Profile" className="profile-pic"></img>
+            <h2>Dr AllahBux Mahar</h2>
+            <h4>General Physician</h4>
+            <a href="#"><p>drallahbux.website</p></a>
+            <button className="email-btn"><img src='images/gmail-logo.png' alt='Gmail logo'></img>Gmail</button>
+            <button className="facebook-btn"><img src='images/facebook-logo.png' alt='Facebook logo'></img>Facebook</button>
         </div>
     )
 }
